35e69835dc9e6fa121863ed84cbda146ccf43975
Christopher Buteau <cbuteau@gmail.com>
2020-08-15 18:28:20 -0400

    headlesS

--- karma.conf.js
+++ karma.conf.js
@@ -70,7 +70,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeExperiment'],
+    browsers: ['ChromeHeadlessNoSandbox'],
 
 
     // Continuous Integration mode
