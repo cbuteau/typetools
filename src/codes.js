cfc410a9c9b4f0b26d75be14d265594255060ee8
Christopher Buteau <cbuteau@gmail.com>
2020-10-12 19:20:15 -0400

    swicthed from int to symbol

--- src/codes.js
+++ src/codes.js
@@ -1,15 +1,15 @@
 
 export const TYPECODES = {
-  BOOLEAN: 0,
-  NUMBER: 1,
-  STRING: 2,
-  FUNCTION: 3,
-  OBJECT: 4,
-  UNDEFINED: 5,
-  NULL: 6,
-  DATE: 7,
-  ARRAY: 8,
-  TYPEDARRAY: 9,
-  REGEX: 10
-  UNMAPPED: 11,
+  BOOLEAN: Symbol('boolean'),
+  NUMBER: Symbol('number'),
+  STRING: Symbol('string'),
+  FUNCTION: Symbol('function'),
+  OBJECT: Symbol('object'),
+  UNDEFINED: Symbol('undefined'),
+  NULL: Symbol('null'),
+  DATE: Symbol('date'),
+  ARRAY: Symbol('array'),
+  TYPEDARRAY: Symbol('typedarray'),
+  REGEX: Symbol('regex'),
+  UNMAPPED: Symbol('unmapped'),
 };
